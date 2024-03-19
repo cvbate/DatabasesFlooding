9ee5d8327aeb4335f3c598ee187825baaa2de289
cvbate <cbate@clarku.edu>
2024-03-19 12:23:00 -0400

    update for analysis_a1 outline for tables/database

--- analysis_A1.sql
+++ analysis_A1.sql
@@ -12,18 +12,17 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 -------- setup
 -- create table for topography
 CREATE TABLE topography(
-    elevation datatype,
-    column2 datatype,
-    column3 datatype,
-   ....
+    elevation varchar(255)
 );
 -- create table for flood events/flood risks
 CREATE TABLE  flood_risk(
-
+flood_area_ID int PRIMARY KEY,
+Risk varchar(255)
 );
 
 -- create table for flood defenses
 CREATE TABLE flood_defenses(
-
-
+defense_ID int PRIMARY KEY,
+defense_type varchar(255),
+GeoLocation GEOMETRY(Point, 4326)
 );
