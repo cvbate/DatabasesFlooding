4fbd15dcccf2a559d11d4e93ebdd9a66e871b43e
cvbate <cbate@clarku.edu>
2024-03-19 12:47:01 -0400

    syntax error fixed - hopefully

--- README.md
+++ README.md
@@ -7,6 +7,12 @@
 
 ### What is included in this reposotory
 
+1. README.md (this document)
+1. INSTRUCTIONS.md: a document that lists the instructions for each assignment
+1. analysis_A1: the sql code for assingment 1 this includes the creation of the database and initial tables
+1. analysis_A2: the sql code for assignment 2
+1. analysis_A3: the sql code for assignment 3
+
 ### Data
 
 ### Assignment 1
--- analysis_A1.sql
+++ analysis_A1.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE "Flooding";
 
 -- Connect to RealEstateDB
-\c flooding
+\c Flooding
 
 -- Enable PostGIS
 CREATE EXTENSION IF NOT EXISTS postgis;
@@ -10,14 +10,17 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 ------- everything above will go in README.md 
 
 -------- setup
--- create table for topography
+-- create table for topography which will be point data
 CREATE TABLE topography(
-elevation varchar(255)
+elevation varchar(255),
+GeoLocation GEOMETRY(Point, 4326)
 );
+
 -- create table for flood events/flood risks
 CREATE TABLE  flood_risk(
 flood_area_ID int PRIMARY KEY,
-Risk varchar(255)
+Risk varchar(255),
+GeoLocation GEOMETRY(Polygon, 4326) -- this should be polygon? check syntax
 );
 
 -- create table for flood defenses
