c858cc4c5908fb0b0524897daff6081702fab2a8
cvbate <cbate@clarku.edu>
2024-03-28 16:17:18 -0400

    finished dataimport_A2 comments

--- dataimport_A2.sql
+++ dataimport_A2.sql
@@ -8,17 +8,18 @@ CREATE EXTENSION IF NOT EXISTS postgis_raster;
 --in command prompt set your path to the folder that contains the data
 cd C:\Users\cvale\DatabaseFlooding\DatabasesFlooding\Data2
 
---  shapefiles used
+-- shapefiles used
 -- LeveeStationsWGS84.shp
 -- BoreholesWGS84.shp
 -- FloodwallsWGS84.shp
 
 ---------------Step 1: use wsl from command prompt
--- use shp2pgsql and raster2pgsql to save vector shapefiles and raster tifs as sql scripts
+-- make sure you are in the directory/folder where the shapefiles files live
 
-
+-- use shp2pgsql and raster2pgsql to save vector shapefiles and raster tifs as sql scripts in your current directory
 -- vectors
--- -I 
+-- -s flag specifies the spatial reference of the source file
+-- -I flag creates an index which speeds up sptial querying
 shp2pgsql -s 4326 -I LeveeStationsWGS84.shp public.LeveeStations_vector > LeveeStations.sql
 
 shp2pgsql -s 4326 -I BoreholesWGS84.shp public.Boreholes_vector > Boreholes.sql
@@ -27,9 +28,10 @@ shp2pgsql -s 4326 -I FloodwallsWGS84.shp public.Floodwalls_vector > Floodwalls.s
 
 
 -- rasters
--- -I 
--- -C
--- -M 
+-- -s flag specifies the spatial reference of the source file
+-- -I flag creates an index on the raster column which speeds up spatial querying
+-- -C flag adds raster contraints to use that the data is imported in the correct format
+-- -M flag vacuum analyze the raster file after loading
 raster2pgsql -s 4326 -I -C -M Katrina_floods_without_perm_water_clip.tif public.katflood_rast > katflood.sql 
 
 raster2pgsql -s 4326 -I -C -M Katrina_Perm_water_clip.tif public.permwater_rast > permwater.sql 
@@ -38,11 +40,12 @@ raster2pgsql -s 4326 -I -C -M katrina_duration_clip.tif public.duration_rast > d
 
 ---------------Step 2: import sql files using command prompt
 
--- -U tag  
--- -d points to the database 
--- -f points to the .sql file
+-- -U connects to the database as a specific user
+-- -d points to the destiniation database
+-- -f points to the .sql file to be imported
 
-C:\Users\cvale\DatabaseFlooding\DatabasesFlooding\Data2> "c:\Program Files\PostgreSQL\16\bin\psql.exe" -U postgres -d Flooding -f LeveeStations.sql
+-- make sure you are in the directory/folder where the .sql files live
+"c:\Program Files\PostgreSQL\16\bin\psql.exe" -U postgres -d Flooding -f LeveeStations.sql
 "c:\Program Files\PostgreSQL\16\bin\psql.exe" -U postgres -d Flooding -f Boreholes.sql
 "c:\Program Files\PostgreSQL\16\bin\psql.exe" -U postgres -d Flooding -f Floodwalls.sql
 
